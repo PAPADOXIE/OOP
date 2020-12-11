51c615df4ebfbf5304de33ac6e14554cb1099e89
Waleed Butt <waleedb403@gmail.com>
2020-12-11 15:09:40 +0500

    jojojojojojojojojojojojojo

--- GroupLab04/include/Department.h
+++ GroupLab04/include/Department.h
@@ -4,11 +4,11 @@
 class Department{
 protected:
     std::string Title;
-    int no_of_possible_Employies;
-    int current_no_of_Employies;
+    int no_of_possible_Employees;
+    int current_no_of_Employees;
     Employee** DIO;
 public:
-Department(std::string Title,int no_of_possible_Employies);
+Department(std::string Title,int no_of_possible_Employees);
 void addEmployee(Employee Jojo,int Type);
 void modifyEmployee(int Employee_No,int Modified_Salary);
 void calcSalary(int Employee_No);
--- GroupLab04/source/Department.cpp
+++ GroupLab04/source/Department.cpp
@@ -1,5 +1,8 @@
 #include "../include/Department.h"
-Department::Department(std::string Title,int no_of_possible_Employies){
+Department::Department(std::string Title,int no_of_possible_Employees){
     this->Title=Title;
-    this->no_of_possible_Employies
+    this->no_of_possible_Employees=no_of_possible_Employees;
+    current_no_of_Employees=0;
+    DIO = new Employee*[this->no_of_possible_Employees];
+    DIO={NULL};
 }
